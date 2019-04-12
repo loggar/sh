4f98b86bf4291268aa226a396377966c4bcea74c
charly.lee <webnl@DT-Charly.koi.local>
2019-04-12 14:26:58 +1000

    feat (date)

---
+++ sh-core/date.ex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TODAY=$(date +"Today is %A, %d of %B")
+TIMENOW=$(date +"The local time is %r")
+TIME_UK=$(TZ=BST date +"The time in the UK is %r")
+
+echo $TODAY
+echo $TIMENOW
+echo $TIME_UK
